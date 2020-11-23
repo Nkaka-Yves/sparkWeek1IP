72c52c29a436c201724bedcced7bfc55c1baebdd
Nkaka-Yves <nkakayves515@gmail.com>
2020-11-23 08:57:08 +0200

    Adding project content

--- src/test/java/Cypher/DicypherTest.java
+++ src/test/java/Cypher/DicypherTest.java
@@ -1,3 +1,18 @@
+package Cypher;
+
+import Cypher.Cypher;
+import org.junit.Test;
+import Cypher.Dicypher;
+
+import static org.junit.Assert.assertEquals;
+
 public class DicypherTest {
+
+    @Test
+    public void testIfDecryptionWork(){
+       Dicypher.Dicypher("pmcmc",2);
+        String expectedDescription = "nkaka";
+        assertEquals(expectedDescription,Dicypher.Dicypher("pmcmc",2));
+    }
 
 }
